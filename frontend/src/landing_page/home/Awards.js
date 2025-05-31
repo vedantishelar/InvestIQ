b939805d0b4ad4273f725fd38a995f99a9b78432
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-05-31 20:24:09 +0530

    Done with sections in Home component

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -2,7 +2,46 @@ import React from 'react';
 
 function Awards() {
     return ( 
-        <h1>Awards</h1>
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-6 p-5'>
+                    <img src="media/images/largestBroker.svg" alt="BrokerImage" className="mb-5"/>
+                </div>
+                <div className='col-6 p-5 mt-5'>
+                    <h1>Largest stock broker in India</h1>
+                    <p className='mb-5'> 2+ million InvestIQ clients contribute to over 15% of all retail order <br/>volumes in India daily by trading and investing in:</p>
+                    <div className='row'>
+                        <div className='col-6'>
+                          <ul>
+                            <li>
+                                <p>Futures and Options</p>
+                            </li>
+                            <li>
+                                <p>Commodity derivatives</p>
+                            </li>
+                            <li>
+                                <p>Currency derivatives</p>
+                            </li>
+                          </ul>
+                        </div>
+                        <div className='col-6'>
+                          <ul>
+                            <li>
+                                <p>Stocks & IPOs</p>
+                            </li>
+                            <li>
+                                <p>Direct mutual funds</p>
+                            </li>
+                            <li>
+                                <p>Bonds and Govt. Securities</p>
+                            </li>
+                          </ul>
+                        </div>
+                    </div>
+                    <img src="media/images/pressLogos.png" alt="BrokerImage" style={{width: "90%"}}/>
+                </div>
+            </div>
+        </div>
      );
 }
 

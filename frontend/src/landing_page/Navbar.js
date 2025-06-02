eec81938b4e8778f890ee5d2695514806053e9c5
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-06-02 11:08:56 +0530

    Done With Navbar

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -3,10 +3,10 @@ import React from "react";
 function Navbar() {
   return (
     
-      <nav className="navbar navbar-expand-lg bg-light bg-body-tertiary">
-        <div className="container-fluid">
+      <nav className="navbar navbar-expand-lg border-bottom" style={{backgroundColor: "#FFF;"}}>
+        <div className="container p-2">
           <a className="navbar-brand" href="/">
-            <img src="media/images/logo.png" style={{width: "21%"}} alt="logo"/>
+            <img src="media/images/logo.png" style={{width: "18%"}} alt="logo"/>
           </a>
           <button
             className="navbar-toggler"
@@ -20,64 +20,40 @@ function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+    
+            <form className="d-flex" role="search">
+               <ul className="navbar-nav mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
-                  Home
+                  Signup
                 </a>
               </li>
+
               <li className="nav-item">
-                <a className="nav-link" href="/link">
-                  Link
+                <a className="nav-link active" href="/link">
+                  About
                 </a>
               </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/dropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu">
-                  <li>
-                    <a className="dropdown-item" href="/action">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/another-action">
-                      Another action
-                    </a>
-                  </li>
-                  <li>
-                    <hr className="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/something-else">
-                      Something else here
-                    </a>
-                  </li>
-                </ul>
-              </li>
+
               <li className="nav-item">
-                <span className="nav-link disabled" aria-disabled="true">
-                  Disabled
-                </span>
+                <a className="nav-link active" href="/link">
+                  Product
+                </a>
               </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" href="/link">
+                  Pricing
+                </a>
+              </li>
+
+              <li className="nav-item">
+                <a className="nav-link active" href="/link">
+                  Support
+                </a>
+              </li>
+
             </ul>
-            <form className="d-flex" role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
             </form>
           </div>
         </div>

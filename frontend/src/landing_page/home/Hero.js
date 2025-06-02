249d3626d63c33e70e0a2151f32f04e48cdd0e84
Vedanti Shelar <shelarvedanti485@gmail.com>
2025-06-02 12:36:22 +0530

    New changes

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -7,7 +7,7 @@ function Hero() {
             <img src="media/images/homeHero.png" alt="HeroImage" className="mb-5"/>
             <h1 className="mt-5">Invest in everything</h1>
             <p>Online platform to invest in stocks, derivatives, mutual funds, and more</p>
-            <button className="p-2 btn btn-primary fs-5 mb-5" style={{width:"17%", margin: "0 auto"}}>Signup Now</button>
+            <button className="p-2 btn btn-primary fs-5 mb-5" style={{width:"18%", margin: "0 auto"}}>Signup Now</button>
         </div>
       </div>
   );
